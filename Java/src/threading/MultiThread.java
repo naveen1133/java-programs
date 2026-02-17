69af1a8783e765b9081cad4a155a3288f4eb6ad6
Naveen <ynaveen1133gmail.com>
2026-02-17 09:20:21 +0530

    multi thread

---
+++ Java/src/threading/MultiThread.java
@@ -0,0 +1,12 @@
+package threading;
+
+public class MultiThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 MyThread t = new MyThread();
+	        t.start();
+
+	}
+
+}
